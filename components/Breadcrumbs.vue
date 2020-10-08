4bd74de0a4565467ea58a6b1749bb5f7081effba
DmitriyKurapin <dmitriy.kurapin@callvu.com>
2020-10-08 22:25:13 +0300

    improved translation of breadcrumbs

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -1,15 +1,13 @@
-<!-- eslint-disable max-len -->
-
 <template>
   <nav class="my-6" aria-label="Breadcrumb">
     <ol class="list-none p-0 text-xs inline-flex">
       <li
         v-for="(bData, idx) in breadcrumbsList"
-        :key="`${bData.label}_${idx}`"
+        :key="`breadcrumbs_label_${idx}`"
         :class="['flex items-center', bData.isLastItem ? 'text-primary font-bold text-sm' : null]"
       >
         <nuxt-link :to="localePath(bData.link)">
-          {{ bData.isLastItem ? bData.label : $t(`${bData.label}`) }}
+          {{ bData.label }}
         </nuxt-link>
         <svg
           v-if="!bData.isLastItem"
@@ -19,7 +17,12 @@
           viewBox="0 0 320 512"
         >
           <path
-            d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+            :d="
+              `M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941
+               0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256
+               34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941
+               0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z`
+            "
           />
         </svg>
       </li>
@@ -39,12 +42,18 @@ export default defineComponent({
       $toKebabCase
     } = ctx.root;
     const breadcrumbsList: NavData[] = [
-      { link: '/', label: 'navbar.home_page', subCategories: [], isLastItem: false },
+      { link: '/', label: ctx.root.$t('navbar.home_page'), subCategories: [], isLastItem: false },
       ...Object.values(routeParams).reduce((res, curr, idx, srcArr) => {
         const isLastItem = typeof srcArr[idx + 1] === 'undefined';
+        let itemLabel = ctx.root.$t(`navbar.${$toKebabCase(curr)}`);
+        // check if current translation is existed
+        if (itemLabel === `navbar.${$toKebabCase(curr)}`) {
+          itemLabel = curr;
+        }
+
         res.push({
           link: `${idx === 0 ? '' : res[idx - 1].link}/${curr}`,
-          label: isLastItem ? curr : `navbar.${$toKebabCase(curr)}`,
+          label: itemLabel,
           subCategories: [],
           isLastItem
         });
