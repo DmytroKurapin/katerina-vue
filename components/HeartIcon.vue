df13ecc47fc8abeb2f14a1b8f118fdba16ca8b3c
DmitriyKurapin <dmitriy.kurapin@callvu.com>
2020-11-10 17:48:15 +0200

    fixed click event on favorites icon

--- components/HeartIcon.vue
+++ components/HeartIcon.vue
@@ -4,7 +4,7 @@
     class="cursor-pointer fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 20 20"
-    @click.prevent="$emit('click')"
+    @click="$emit('click')"
   >
     <path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z" />
   </svg>
