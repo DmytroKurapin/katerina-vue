772757048ecbd15f40f0f52611fa2b55ec36b090
DmitriyKurapin <dmitriy.kurapin@callvu.com>
2020-11-14 16:10:25 +0200

    set uppercase info block text; reduce size of close icon

--- components/InformationalBlock.vue
+++ components/InformationalBlock.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="!isInfoBlockHidden" class="bg-primary-light px-8 py-3 relative text-center z-20">
-    <span class="me-4">{{ $t('general.info_text') }}</span>
+    <span class="uppercase me-4">{{ $t('general.info_text') }}</span>
 
     <!-- close icon -->
     <button type="button" class="absolute h-full top-0 end-4 focus:outline-none" @click="hideInfoBlock">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="w-5 h-5 fill-current">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="w-3 h-3 fill-current">
         <path
           :d="
             `M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071
